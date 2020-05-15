85f251c73a56e07db347e6bb8d4d3f74bc43a603
zhurik <zhyriki@ukr.net>
2020-05-16 00:53:14 +0300

    create webpack config

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import './module'
+import './sass/index.sass'
+
+console.log('js working')
+
+/**
+ * Add two numbers.
+ * @param {number} num1 The first number.
+ * @param {number} num2 The second number.
+ */
+function some() {}
+
+some()
